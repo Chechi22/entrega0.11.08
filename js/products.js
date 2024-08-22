7ae24b4a4c09545c4bae882413826882623a39ba
Vic Avril <victoria.avril.carrasco@fing.edu.uy>
2024-08-22 11:46:32 -0300

    muestra los elementos de json

---
+++ js/products.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function(){
+    fetch("https://japceibal.github.io/emercado-api/cats_products/101.json")
+    .then(response=>response.json())
+    .then(autos=>{
+
+        let listaautos="";
+        for(let auto of autos.products){
+            listaautos+="<table><tr><td rowspan=4><img src="+ auto.image +"></td><td>"+ auto.name +"</td></tr><tr><td>Descripción: "+ auto.description
+            +"</td></tr><tr><td>"+auto.currency+" "+auto.cost+"</td></tr><tr><td>Vendidos: "+auto.soldCount+"</td></tr></table>";
+        }
+        document.getElementById("autos").innerHTML=listaautos;
+
+    })
+})
+
